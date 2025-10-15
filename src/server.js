23bff6b47abdd52c3d9fc205193f78fd97ca674c
Arda Tinmaz <tnmzarda@gmail.com>
2025-10-15 20:33:59 +0300

    Server.js Fix

--- src/server.js
+++ src/server.js
@@ -3,23 +3,23 @@ const cors = require('cors');
 const pinoHttp = require('pino-http');
 
 const contactsRouter = require('./routers/contacts');
+const authRouter = require('./routers/auth');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { notFoundHandler } = require('./middlewares/notFoundHandler');
-const authRouter = require('./routers/auth')
-app.use('/auth', authRouter);
-
 
 function setupServer() {
-  const app = express();
+  const app = express(); 
 
   app.use(cors());
   app.use(pinoHttp());
   app.use(express.json());
 
-  // (Opsiyonel) Basit health/info
-  app.get('/', (_req, res) => res.json({ ok: true, routes: ['/contacts', '/contacts/:contactId'] }));
-
+  
+  app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
+
+  // Basit health/info endpoint
+  app.get('/', (_req, res) => res.json({ ok: true, routes: ['/auth', '/contacts'] }));
 
   // 404 ve hata yakalayıcılar
   app.use(notFoundHandler);
